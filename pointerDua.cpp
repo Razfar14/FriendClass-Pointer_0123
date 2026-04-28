53fb9cbebe5a00967ff35d691ca015578015e124
Razfar14 <rajarangkuty14@gmail.com>
2026-04-28 15:29:29 +0700

    memulai

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
 using namespace std;
 
-class mahasiwa{
-    public:
-        int nim;
-        void showNim(){
-            cout << "No Induk: " << nim << endl;
-        }
-};
 
-int main(){
-    mahasiwa *mhs= new mahasiwa{1};
-    mhs->nim = 2;
-    mhs->showNim();
-    delete mhs;
-    return 0;
-}
